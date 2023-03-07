58b0c81ce1d9e4a80bd506578f40dfcdd6467fff
mr cod3r <mrcod3r.ir@gmail.com>
2023-03-07 15:07:55 +0330

    add view pages

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-  return render(request,'blog/index.html',)
+  return render(request,'blog/index.html')
+
+def about(request):
+  return render(request,'blog/about.html')
+
+def contact(request):
+  return render(request,'blog/contact.html')
+
